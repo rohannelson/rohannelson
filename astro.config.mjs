8faa63793f43703f62760e6354bc9b25d3a0d610
Rohan Nelson <rohan.nelson@gmail.com>
2023-03-20 14:46:12 +0800

    changed config site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "http://localhost:3000"
+    site: "https://rohannelson.com"
 });
