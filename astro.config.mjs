da785d39a79ec23fe20a80a683697c0823594b58
Rohan Nelson <rohan.nelson@gmail.com>
2023-04-18 08:51:43 +0800

    Changed Google Analytics to work with partytown due to it not working. Will see how this goes...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rohannelson.com",
-  integrations: [mdx()]
+  integrations: [mdx(), partytown()]
 });
