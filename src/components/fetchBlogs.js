3c813d0284663fb2a5acdb88506243d9b6113279
Rohan Nelson <rohan.nelson@gmail.com>
2023-07-11 13:27:34 +0800

    succesfully migrated to Strapi, including parsing of remote markdown via @astrojs/markdown-component

--- src/components/fetchBlogs.js
+++ src/components/fetchBlogs.js
@@ -3,6 +3,6 @@ const blogsG = await fetchApi({
     endpoint: "blogs",
     wrappedByKey: "data",
 });
-const blogsArrayG = [];
-blogsG.forEach((blog) => blogsArrayG.push(blog.attributes));
-export default blogsArrayG;
+const blogsArray = [];
+blogsG.forEach((blog) => blogsArray.push(blog.attributes));
+export default blogsArray;
