273e68b286b8888ea233becc900585f66e820b92
Rohan Nelson <rohan.nelson@gmail.com>
2023-09-27 22:30:04 +0800

    rss feed working! (Needs styling)

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,12 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss'
 
-export async function GET(context) {
+export async function get(context) {
     return rss({
         title: "Rohan Nelson's Blog",
         description: "Sharing and reflecting on my own experiences.",
         site: context.site,
         items: await pagesGlobToRssItems(
-            import.meta.glob('./blog/*.{mdx}'),
+            import.meta.glob('./blog/*.mdx')
         ),
     });
 }
