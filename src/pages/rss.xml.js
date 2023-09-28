9b64edd0cf9ad1d77d9cb7188f98df40dc749782
Rohan Nelson <rohan.nelson@gmail.com>
2023-09-28 20:11:06 +0800

    testing styled rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,5 +8,8 @@ export async function get(context) {
         items: await pagesGlobToRssItems(
             import.meta.glob('./blog/*.mdx')
         ),
+        customData: '<languauge>en-au</language>',
+        stylesheet: '/pretty-feed-v3.xsl',
+        xmlns: { h: 'http://www.w3.org/TR/html4/' },
     });
 }
