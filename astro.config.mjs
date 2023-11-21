e8fb1182e5ba70850f696883d45292644a6c291c
Rohan Nelson <rohan.nelson@gmail.com>
2023-11-22 06:41:04 +0800

    New post 'Where to from here'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import partytown from "@astrojs/partytown";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rohannelson.com",
-  integrations: [mdx(), partytown(), react()]
+  integrations: [mdx(), react()],
 });
