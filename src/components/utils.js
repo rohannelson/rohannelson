441e848ae46546608c22f09a32753932c99479d4
Rohan Nelson <rohan.nelson@gmail.com>
2023-11-22 18:39:58 +0800

    Added series descripitons and fixed capitalisation on series cards.

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,3 +1,10 @@
 export const capitalise = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  };
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export const capitaliseAll = (str) => {
+  return str
+    .split(" ")
+    .map((word) => capitalise(word))
+    .join(" ");
+};
