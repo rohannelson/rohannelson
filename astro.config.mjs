4c45edc5365b19f7295a215d49508c8e90a5d1d8
Rohan Nelson <rohan.nelson@gmail.com>
2023-12-09 06:00:25 +0800

    Migrated to astro-icon@next due to incompatibility of old version with SSR. Series index page no longer working due to loss of Astro.props... may need to migrate to Astro's content collection feature?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-
+import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rohannelson.com",
-  integrations: [mdx(), react()],
+  integrations: [mdx(), react(), icon()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
