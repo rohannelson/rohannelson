53a773ff39cc73df3196fb29a00558c4b731e5f5
Rohan Nelson <rohan.nelson@gmail.com>
2023-12-09 05:02:10 +0800

    Added Clouflare SSR adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://rohannelson.com",
   integrations: [mdx(), react()],
+  output: "server",
+  adapter: cloudflare()
 });
