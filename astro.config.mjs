97946f9a48e36d02d4b38955316b9f4c09acb6d4
Rohan Nelson <rohan.nelson@gmail.com>
2023-12-21 13:48:53 +0800

    Tailwind setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import react from "@astrojs/react";
 import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://rohannelson.com",
-  integrations: [mdx(), react(), icon()],
+  integrations: [mdx(), react(), icon(), tailwind()],
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare()
 });
