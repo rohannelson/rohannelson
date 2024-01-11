6d741b1b4b044cae0f2a5657439b5836a49b657f
Rohan Nelson <rohan.nelson@gmail.com>
2024-01-11 20:38:12 +0800

    Fixed production anomaly where heading styles are overridden on blog pages. CSS output order was a bit random. Had to move styles (including Tailwind ones) from baselayout.astro to main.css and switch off Tailwind base styles in Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
-import icon from "astro-icon";
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
+import icon from 'astro-icon'
+import cloudflare from '@astrojs/cloudflare'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rohannelson.com",
-  integrations: [mdx(), react(), icon(), tailwind()],
-  output: "server",
-  adapter: cloudflare()
-});
+	site: 'https://rohannelson.com',
+	integrations: [mdx(), react(), icon(), tailwind({ applyBaseStyles: false })],
+	output: 'server',
+	adapter: cloudflare()
+})
