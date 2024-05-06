1a0a7205783f42ade52833fdc602aebed8e79afa
Rohan Nelson <rohan.nelson@gmail.com>
2024-05-06 10:47:18 +0800

    Subscribe form fully functional and styled, present at bottom of blog posts. Yet to implement any bot-safety measures etc... Also made a minor css change to blog navigation card (NavCard) to make it more responsive.

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -3,11 +3,9 @@ import type { APIRoute } from 'astro'
 
 export const POST: APIRoute = async ({ request, redirect }) => {
 	const formData = await request.formData()
-	console.log(formData)
 	const firstName = formData.get('firstName')
 	const lastName = formData.get('lastName')
 	const email = formData.get('email')
-	console.log(firstName, lastName, email)
 	let contactsApi = new Brevo.ContactsApi()
 	contactsApi.setApiKey(Brevo.ContactsApiApiKeys.apiKey, import.meta.env.BREVO_API)
 	let createContact = new Brevo.CreateContact()
@@ -17,7 +15,12 @@ export const POST: APIRoute = async ({ request, redirect }) => {
 	createContact.attributes = { FIRSTNAME: firstName, LASTNAME: lastName, OPT_IN: true }
 	const { response, body } = await contactsApi.createContact(createContact)
 	if (response) {
-		return new Response(JSON.stringify(response.statusCode, response.statusMessage, body))
+		const responseBody = {
+			statusCode: response.statusCode,
+			statusMessage: response.statusMessage,
+			body: JSON.stringify(body)
+		}
+		return new Response(JSON.stringify(responseBody))
 	}
 	return new Response('error')
 }
